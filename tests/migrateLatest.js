ea2e7da5ca187aabc1cdcc6e7c351d6b3b82ae64
Andrei Lucas Gonçalves <andrei.lucas.goncalves@hotmail.com>
2021-02-14 14:58:55 -0300

    Added env file

--- tests/migrateLatest.js
+++ tests/migrateLatest.js
@@ -1,7 +1,16 @@
 const migrateLatest = require('../dist/index').default;
+require('dotenv').config();
 
 describe('executing migrateLatest', () => {
   it('should be executing', async () => {
-    await migrateLatest({}, 'tests/migrations')
+    await migrateLatest(
+      {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+      },
+      'tests/migrations'
+    );
   });
 });
